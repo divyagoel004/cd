a4ea55795686097d8854056d6baa3e624589406c
divyagoel004 <157902097+divyagoel004@users.noreply.github.com>
2025-05-06 12:17:46 +0530

    Update index.py

--- index.py
+++ index.py
@@ -19,4 +19,5 @@ def home():
 
 if __name__ == "__main__":
     # Local dev run
-    app.run(debug=True , host='0.0.0.0',port=3000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render-provided PORT
+    app.run(host="0.0.0.0", port=port, debug=True)
