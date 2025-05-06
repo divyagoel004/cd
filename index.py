4c243f2f13c4983eca52e218d3d4e750761bc161
divyagoel004 <157902097+divyagoel004@users.noreply.github.com>
2025-05-06 14:49:14 +0530

    Update index.py

--- index.py
+++ index.py
@@ -19,5 +19,5 @@ def home():
 
 if __name__ == "__main__":
     # Local dev run
-    port = int(os.environ.get("PORT", 10000))  # Fall back to 10000 locally
+    port = int(os.environ.get("PORT", 5058))  # Fall back to 10000 locally
     app.run(host="0.0.0.0", port=port, debug=True)
